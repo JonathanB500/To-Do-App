9e0b655e74821392b848e2611127523418dcfef0
Jonathan Buitrago Roncancio <jonathanbuitrago505@gmail.com>
2024-08-28 10:56:00 -0500

    Update add item color

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -102,7 +102,7 @@ function AddItemForm({ onNewItem }) {
                 <InputGroup.Append>
                     <Button
                         type="submit"
-                        variant="primary"
+                        variant="danger"
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
